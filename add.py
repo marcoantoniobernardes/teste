754ef764529cc67345614ed0d4f04985ab38e3ea
Marco Antônio Bernardes Saraiva <mabsaraiva1000@gmail.com>
2022-08-21 05:56:25 -0300

    add.py Hello word

--- add.py
+++ add.py
@@ -1,15 +1,18 @@
-#1
+# 1
 # # coding: utf-8
 from flask import Flask, render_template
 app = Flask("projeto")
+
+
 @app.route("/")
 def ola_mundo():
-    #criar uma variável com o meu nome
-    nome="Marco Antonio"
+    # criar uma variável com o meu nome
+    nome = "Marco Antonio"
     produtos = [
-    {"nome": "Ração", "preco": 199.99},
-    {"nome": "Playstation", "preco": 1999.99}]
+        {"nome": "Ração", "preco": 199.99},
+        {"nome": "Playstation", "preco": 1999.99}]
 
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
+
 
 app.run()
